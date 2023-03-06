c226b433befd800c05fc2e15a82746fe374b57cd
Paul Paniagua <70442497+pauljosepq@users.noreply.github.com>
2023-03-06 19:16:36 -0400

    Expand commit types
    * feat: Add new commit types

--- src/commit-types.js
+++ src/commit-types.js
@@ -1,37 +1,52 @@
 export const COMMIT_TYPES = {
-    feat: {
-        emoji: '🚀',
-        description: 'Add new feature',
-        release: true // que si haces un commit de este tipo, luego deberías hacer un release
-    },
-    fix: {
-        emoji: '🐛',
-        description: 'Submit a fix to a bug',
-        release: true
-    },
-    perf: {
-        emoji: '⚡️',
-        description: 'Improve performance',
-        release: true
-    },
-    ref: {
-        emoji: '⚒️',
-        description: 'Refactor code',
-        release: true
-    },
-    docs: {
-        emoji: '📕',
-        description: 'Add or update documentation',
-        release: false
-    },
-    test: {
-        emoji: '🧪',
-        description: 'Add or update tests',
-        release: false
-    },
-    build: {
-        emoji: '🏗️',
-        description: 'Add or update build scripts',
-        release: false
-    }
+  feat: {
+    emoji: '🆕',
+    description: 'Add new feature',
+    release: true
+  },
+  fix: {
+    emoji: '🐛',
+    description: 'A bug fix',
+    release: true
+  },
+  perf: {
+    emoji: '⚡️',
+    description: 'Improve performance',
+    release: true
+  },
+  refactor: {
+    emoji: '⚒️',
+    description: 'Change of existing code without affecting the usage of the application',
+    release: true
+  },
+  build: {
+    emoji: '🏗',
+    description: 'Changes that affect the build system or external dependencies',
+    release: false
+  },
+  revert: {
+    emoji: '🔙',
+    description: 'Revert changes of a previous commit',
+    release: false
+  },
+  docs: {
+    emoji: '📕',
+    description: 'Documentation changes only',
+    release: false
+  },
+  style: {
+    emoji: '💅',
+    description: 'Changes that are related to the style of the document ',
+    release: false
+  },
+  test: {
+    emoji: '🧪',
+    description: 'Add or update tests',
+    release: false
+  },
+  ci: {
+    emoji: '🚀',
+    description: 'Changes to our CI configuration files and scripts',
+    release: false
+  }
 }
